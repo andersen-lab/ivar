f5ef5849498df38a4b0e962dc65369ba220db061
cmaceves <christineaceves22@gmail.com>
2022-11-21 15:24:05 -0800

    pushing fix to not try and use internal cluster as external cluster in b term calculation of sil score

--- src/clustering.cpp
+++ src/clustering.cpp
@@ -401,8 +401,12 @@ double cluster_point_distances(alglib::real_2d_array X, alglib::kmeansreport rep
 
   //find the average distance from each external cluster
   for(int z=0; z < n_clusters; z++){
+    double zz = z;
+    if(zz == center){
+      continue;
+    }
     tmp = external_dist[z][0] / external_dist[z][1];
-    std::cout << external_dist[z][0] << " " << external_dist[z][1] << " " << tmp << std::endl;
+    std::cout << "z " << z << " " << external_dist[z][0] << " " << external_dist[z][1] << " " << tmp << std::endl;
     //find the minimum of the external cluster dists
     if(tmp < b){
       b = tmp;
