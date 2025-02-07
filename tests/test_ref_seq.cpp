d4e380ae0152c272134706d14ad25eaccb2e5470
gkarthik <xraydiffraction@gmail.com>
2025-02-06 19:16:23 -0800

    Added test_ref_seq.cpp to automated checks.

--- tests/test_ref_seq.cpp
+++ tests/test_ref_seq.cpp
@@ -15,27 +15,27 @@ int main() {
   std::vector<gff3_feature> g = refantd.get_gff_features();
   char *codon = new char[3];
   codon = refantd.get_codon(30, "test", g.at(2));
-  num_success = check_failure('C', 'A', 'T', codon, 30);
+  num_success += check_failure('C', 'A', 'T', codon, 30);
   codon = refantd.get_codon(30, "test", g.at(3));
-  num_success = check_failure('T', 'C', 'A', codon, 30);
+  num_success += check_failure('T', 'C', 'A', codon, 30);
   codon = refantd.get_codon(29, "test", g.at(2));
-  num_success = check_failure('C', 'A', 'T', codon, 29);
+  num_success += check_failure('C', 'A', 'T', codon, 29);
   // After edit_pos
   codon = refantd.get_codon(107, "test", g.at(4));
-  num_success = check_failure('C', 'A', 'A', codon, 107);
+  num_success += check_failure('C', 'A', 'A', codon, 107);
   codon = refantd.get_codon(105, "test", g.at(4));
-  num_success = check_failure('C', 'A', 'T', codon, 105);
+  num_success += check_failure('C', 'A', 'T', codon, 105);
   codon = refantd.get_codon(101, "test", g.at(4));
-  num_success = check_failure('T', 'C', 'T', codon, 101);
+  num_success += check_failure('T', 'C', 'T', codon, 101);
   codon = refantd.get_codon(100, "test", g.at(4));
-  num_success = check_failure('G', 'G', 'A', codon, 100);
+  num_success += check_failure('G', 'G', 'A', codon, 100);
   // 2 AA insertion
   codon = refantd.get_codon(100, "test", g.at(5));
-  num_success = check_failure('G', 'G', 'T', codon, 100);
+  num_success += check_failure('G', 'G', 'T', codon, 100);
   codon = refantd.get_codon(101, "test", g.at(5));
-  num_success = check_failure('C', 'A', 'A', codon, 101);
+  num_success += check_failure('C', 'A', 'A', codon, 101);
   codon = refantd.get_codon(140, "test", g.at(5));
-  num_success = check_failure('C', 'T', 'A', codon, 140);
+  num_success += check_failure('C', 'T', 'A', codon, 140);
   std::cout << num_success;
   if (num_success == 0) return 0;
   return -1;
