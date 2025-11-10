9ae8b32197b79fedcf1944fa391e864c91f0d3d9
gkarthik <xraydiffraction@gmail.com>
2025-11-10 11:08:09 -0800

    refactor: removed redundant pos from site_aggregator_stats

--- src/site_aggregator_stats.h
+++ src/site_aggregator_stats.h
@@ -6,17 +6,25 @@
 
 class site_aggregator_stats {
  private:
-  uint32_t pos;
   uint32_t depth=0;
   uint32_t gapped_depth=0;
   uint32_t mean_quality=0;
 
  public:
   void add_site(site_state ss);
-  uint32_t get_pos() const {return pos;};
   uint32_t get_depth() const { return depth; };
   uint32_t get_gapped_depth() const { return gapped_depth; };
   uint32_t get_qual() const { return mean_quality; };
+
+  void set_stats(uint32_t d, uint32_t gd, uint32_t mq){
+    depth = d;
+    gapped_depth = gd;
+    mean_quality = mq;
+  }
+
+  bool operator == (const site_aggregator_stats &other) const {
+    return (depth == other.depth) && (gapped_depth == other.gapped_depth) && (mean_quality == other.mean_quality);
+  }
 };
 
 #endif  // SITE_AGGREGATOR_STATS_H
