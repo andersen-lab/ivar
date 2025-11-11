2c6b0b47817e10a0e96141dad309adfbb1937a4f
gkarthik <xraydiffraction@gmail.com>
2025-11-11 15:12:56 -0800

    refactor: site_aggregator_stats now stores a map from amplicon to site_amplicon_aggregator_stats which stores site-, state-, and amplicon-specific stats.

--- src/site_aggregator_stats.cpp
+++ src/site_aggregator_stats.cpp
@@ -1,25 +1,9 @@
 #include "site_aggregator_stats.h"
 
 void site_aggregator_stats::add_site(site_state ss) {
-  if (ss.state[0] == '-' || ss.state == site_state::GAP) {
-    // Deletion
-    gapped_depth += 1;
-    mean_quality = static_cast<uint8_t>(
-        mean_quality + static_cast<double>(ss.quality - mean_quality) / gapped_depth
-    );
-  } else if (ss.state[0] == '+') {
-    // Insertion
-    depth += 1;
-    gapped_depth += 1;
-    mean_quality = static_cast<uint8_t>(
-        mean_quality + static_cast<double>(ss.quality - mean_quality) / gapped_depth
-    );
-  } else {
-    // SNVs
-    depth += 1;
-    gapped_depth += 1;
-    mean_quality = static_cast<uint8_t>(
-        mean_quality + static_cast<double>(ss.quality - mean_quality) / depth
-    );
-  }
+  site_amplicon_stats[ss.amplicon].add_site(ss);
+}
+
+void site_aggregator_stats::calculate_amplicon_stats() {
+
 }
--- src/site_aggregator_stats.h
+++ src/site_aggregator_stats.h
@@ -1,30 +1,15 @@
-#include <cstdint>
-#include "site_state.h"
+#include <unordered_map>
+#include "interval_tree.h"
+#include "site_amplicon_aggregator_stats.h"
 
 #ifndef SITE_AGGREGATOR_STATS_H
 #define SITE_AGGREGATOR_STATS_H
 
 class site_aggregator_stats {
- private:
-  uint32_t depth=0;
-  uint32_t gapped_depth=0;
-  uint32_t mean_quality=0;
-
  public:
+  std::unordered_map<ITNode*, site_amplicon_aggregator_stats> site_amplicon_stats;
   void add_site(site_state ss);
-  uint32_t get_depth() const { return depth; };
-  uint32_t get_gapped_depth() const { return gapped_depth; };
-  uint32_t get_qual() const { return mean_quality; };
-
-  void set_stats(uint32_t d, uint32_t gd, uint32_t mq){
-    depth = d;
-    gapped_depth = gd;
-    mean_quality = mq;
-  }
-
-  bool operator == (const site_aggregator_stats &other) const {
-    return (depth == other.depth) && (gapped_depth == other.gapped_depth) && (mean_quality == other.mean_quality);
-  }
+  void calculate_amplicon_stats();
 };
 
 #endif  // SITE_AGGREGATOR_STATS_H
