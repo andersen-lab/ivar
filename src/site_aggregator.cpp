98a7c141272903c6dca783e1d6d91d2964402226
gkarthik <xraydiffraction@gmail.com>
2025-11-11 15:11:51 -0800

    refactor: site_aggregator such that site_aggregator.aggregated_site_states is a map of position-state to site_aggregator_stats

--- src/site_aggregator.cpp
+++ src/site_aggregator.cpp
@@ -9,8 +9,7 @@ void site_aggregator::aggregate(const std::vector<site_state> site_states) {
     // Aggregate site level stats by position, state, and amplicon
     site_aggregator_key key = {
       site_states[i].coordinate,
-      site_states[i].state,
-      site_states[i].amplicon
+      site_states[i].state
     };
     aggregated_site_states[key].add_site(site_states[i]);
   }
--- src/site_aggregator.h
+++ src/site_aggregator.h
@@ -1,9 +1,10 @@
 #include <cstdint>
-#include <unordered_map>
-#include "site_state.h"
-#include "site_aggregator_stats.h"
 #include <functional>
+#include <unordered_map>
+
 #include "hash_utils.h"
+#include "site_aggregator_stats.h"
+#include "site_state.h"
 
 #ifndef SITE_AGGREGATOR_H
 #define SITE_AGGREGATOR_H
@@ -11,40 +12,26 @@
 struct site_aggregator_key {
   site_coordinate coordinate;
   std::string state;
-  ITNode* amplicon;
 
   size_t hash() const {
     size_t h = coordinate.hash();
     h = hash_utils::hash_combine(h, std::hash<std::string>{}(state));
-    if (amplicon != nullptr) {
-      h = hash_utils::hash_combine(h, static_cast<size_t>(amplicon->data->low));
-      h = hash_utils::hash_combine(h, static_cast<size_t>(amplicon->data->high));
-    }
     return h;
   }
 
   bool operator==(const site_aggregator_key& other) const {
-    if (!(coordinate == other.coordinate && state == other.state)) {
-      return false;
-    }
-
-    if (amplicon == nullptr || other.amplicon == nullptr) {
-      return amplicon == other.amplicon;
-    }
-
-    return amplicon->data->low == other.amplicon->data->low &&
-           amplicon->data->high == other.amplicon->data->high;
+    return (coordinate == other.coordinate) && (state == other.state);
   }
 
 };
 
 namespace std {
-template<>
-struct hash<site_aggregator_key> {
-  size_t operator()(const site_aggregator_key& c) const {
-    return c.hash();
-  }
-};
+  template<>
+  struct hash<site_aggregator_key> {
+    size_t operator()(const site_aggregator_key& c) const {
+      return c.hash();
+    }
+  };
 }
 
 class site_aggregator {
