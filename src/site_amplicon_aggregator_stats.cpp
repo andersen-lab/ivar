c7f993fb2f97ac37c9a74f8ac7d22343cdd29131
gkarthik <xraydiffraction@gmail.com>
2025-11-29 17:38:50 -0800

    tests: added test for site_state_aggregator_stats

--- src/site_amplicon_aggregator_stats.cpp
+++ src/site_amplicon_aggregator_stats.cpp
@@ -1,7 +1,7 @@
 #include "site_amplicon_aggregator_stats.h"
 
 void site_amplicon_aggregator_stats::add_site(uint8_t ss_quality) {
-  //Note: site_amplicon_aggregator_stats stores stats for one state and one amplicon
+  //TODO: Add explicit check if site_state.amplicon == this->amplicon
   count += 1;
   mean_quality = static_cast<uint8_t>(
       mean_quality + static_cast<double>(ss_quality - mean_quality) / count
