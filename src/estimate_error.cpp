9c7c3ba22effc2604f73eba9d1ed2d7353d325c4
cmaceves <christineaceves22@gmail.com>
2026-01-12 14:01:21 -0800

    updating error code to use gmm_1d in estimation

--- src/estimate_error.cpp
+++ src/estimate_error.cpp
@@ -1,6 +1,7 @@
 #include "estimate_error.h"
 #include "gmm.h"
 #include "saga.h"
+#include "gmm_1d.h"
 
 std::vector<double> z_score(std::vector<double> data) {
     double mean = calculate_mean(data);
@@ -12,137 +13,104 @@ std::vector<double> z_score(std::vector<double> data) {
     return z_scores;
 }
 
-std::vector<uint32_t>determine_outlier_points(std::vector<double> cluster, double threshold){
-    std::vector<uint32_t> removal_points;
-    std::vector<double> z_scores = z_score(cluster);
-    double mean = calculate_mean(cluster);
-//    double std = calculate_standard_deviation(cluster);
-    for(uint32_t i=0; i < z_scores.size(); i++){
-      double abs = std::abs(z_scores[i]);
-      if(abs >= threshold){
-        std::cerr << "remove " << abs << " " << cluster[i] << std::endl;
-        removal_points.push_back(i);
-      }
+void determine_outlier_points(std::vector<double> cluster, double threshold, std::vector<uint32_t> &removal_points){
+  std::vector<double> z_scores = z_score(cluster);
+  for(uint32_t i=0; i < z_scores.size(); i++){
+    double abs = std::abs(z_scores[i]);
+    if(abs >= threshold){
+      std::cerr << "remove " << abs << " " << cluster[i] << std::endl;
+      removal_points.push_back(i);
     }
-    return(removal_points);
+  }
 }
-
 
 void cluster_error(std::vector<variant> base_variants, uint8_t quality_threshold, uint32_t depth_cutoff, double &error_rate){
   double lower_bound = 0.50;
   double upper_bound = 0.99;
+  const double eps = 1e-6;
   set_freq_range_flags(base_variants, lower_bound, upper_bound, false);
 
-  std::vector<variant> variants_original;
-  uint32_t useful_count_original = 0;
   std::vector<double> frequencies;
-  std::vector<uint32_t> subsample_position;
+  std::vector<uint32_t> sites;
 
   for(uint32_t i=0; i < base_variants.size(); i++){
     if(!base_variants[i].amplicon_flux && !base_variants[i].depth_flag && \
       !base_variants[i].outside_freq_range && !base_variants[i].qual_flag && \
       !base_variants[i].amplicon_masked){
-      useful_count_original++;
-      variants_original.push_back(base_variants[i]);
       frequencies.push_back(base_variants[i].gapped_freq);
-      subsample_position.push_back(base_variants[i].position);
+      sites.push_back(base_variants[i].position);
     }
   }
 
-  if(variants_original.empty()){
-    error_rate = 1;
+  if(frequencies.empty()){
+    error_rate = 0.01;
     return;
   }
-  std::cerr << "error estimate useful var " << useful_count_original << std::endl;
-  arma::mat data_original(1, useful_count_original, arma::fill::zeros);
-  uint32_t count_original=0;
-  for(uint32_t i = 0; i < variants_original.size(); i++){
-    //double tmp = static_cast<double>(variants_original[i].gapped_freq);
-    double tmp = logit(variants_original[i].gapped_freq);
-    data_original.col(count_original) = tmp;
-    count_original += 1;
-  }
 
-  uint32_t chosen_peak = 0;
-  double var_floor = 0.00001;
-  bool clustering_failed = false;
-  uint32_t i = 1;
-  uint32_t optimal_n = 1;
-  uint32_t n = 1;
-  double dcov_threshold = 0.20;
-  bool done_clustering = true;
+  std::vector<double> x_logit;
+  gmm_1d::logit_transform(frequencies, x_logit, eps);
+  int N = 5;
 
-  while(n <= 5){
-    clustering_failed = false;
-    done_clustering = true;
-    reset_variants_info(variants_original);
-    std::cerr << "n " << n << std::endl;
-    gaussian_mixture_model model = retrain_model(n, data_original, variants_original, 2, var_floor, clustering_failed, false);
-    uint32_t largest_idx = std::distance(model.means.begin(), std::max_element(model.means.begin(), model.means.end()));
+  // Fit GMM
+  std::vector<double> logL_history;
+  gmm_1d model(N);
+  model.fit(
+      x_logit,
+      sites,
+      logL_history,
+      20,
+      1e-6,
+      true
+  );
 
-    double target_dcov = model.dcovs[largest_idx];
-    std::cerr << "target " << target_dcov << std::endl;
-    if(target_dcov > dcov_threshold){
-      done_clustering = false;
-    }
-    for(uint32_t i=0; i < model.means.size(); i++){
-      std::cerr << "  means " << model.means[i] << " dcovs " << model.dcovs[i] << std::endl;
-    }
-    if(done_clustering){
-      optimal_n = n;
-      break;
-    }
-    n++;
-  }
-  std::cerr << "optimal n: " << optimal_n << std::endl;
-  gaussian_mixture_model model = retrain_model(optimal_n, data_original, variants_original, 2, var_floor, clustering_failed, false);
-  std::vector<double> means = model.means;
-  for(auto m : means){
-    std::cerr << "mean " << m << std::endl;
-  }
-  std::vector<std::vector<double>> clusters(optimal_n);
-  for(uint32_t i=0; i < variants_original.size(); i++){
-    double freq = variants_original[i].gapped_freq;
-    std::size_t best_idx = 0;
-    double best_dist = std::numeric_limits<double>::max();
-    for (std::size_t j = 0; j < means.size(); ++j) {
-        double dist = std::abs(freq - means[j]);
-        if (dist < best_dist) {
-            best_dist = dist;
-            best_idx = j;
-        }
-    }
-    clusters[best_idx].push_back(freq);
-    //std::cerr << freq << " " << means[best_idx] << std::endl;
-    //exit(0);
-  }
+  model.get_distinct_components_count(sites);
+
+  std::cerr << "Merged components: " << model.merged_means.size() << "\n";
   
+  uint32_t final_N = model.merged_means.size();
+  gmm_1d model2(final_N);
+  model2.fit(
+      x_logit,
+      sites,
+      logL_history,
+      20,
+      1e-6,
+      true
+  );
+  const auto& m2 = model2.get_means();
+  std::vector<double> m_sigmoid2;
 
-  chosen_peak = std::distance(means.begin(), std::max_element(means.begin(), means.end()));
-  std::vector<double> cleaned_cluster;
-  std::vector<uint32_t> outliers;
-
-  //for each cluster this describes the points which are outliers
-  if(optimal_n > 1){
-    std::vector<double> universal_cluster = clusters[chosen_peak];
-    //outliers = determine_outlier_points(universal_cluster, 2.5);
-    for(uint32_t i=0; i < universal_cluster.size(); i++){
-      auto it = std::find(outliers.begin(), outliers.end(), i);
-      if(it == outliers.end()){
-        cleaned_cluster.push_back(universal_cluster[i]);
-      }
+  gmm_1d::sigmoid_transform(m2, m_sigmoid2, eps);
+  double largest_mean = 0.0;
+  uint32_t largest_idx = 0;
+  for(uint32_t m=0; m < m_sigmoid2.size(); ++m){
+    std::cerr << "mean " << m_sigmoid2[m] << "\n";
+    if(m_sigmoid2[m] > largest_mean){
+      largest_mean = m_sigmoid2[m];
+      largest_idx = m;
     }
-  } else {
-    //TODO handle the case of n=1
-    outliers = determine_outlier_points(frequencies, 2.5);
-    for(uint32_t i=0; i < frequencies.size(); i++){
-      auto it = std::find(outliers.begin(), outliers.end(), i);
-      if(it == outliers.end()){
-        cleaned_cluster.push_back(frequencies[i]);
-      }
+  }
+  std::vector<uint32_t> assigned_components;
+  std::vector<std::vector<double>> marginal_posterior_probabilities;
+  model2.predict(
+      x_logit,
+      sites,
+      assigned_components,
+      marginal_posterior_probabilities
+  );
+
+  std::vector<double> largest_cluster_snps;
+  for(uint32_t i =0; i < frequencies.size(); i++){
+    if(assigned_components[i] == largest_idx){
+      largest_cluster_snps.push_back(frequencies[i]);
     }
   }
 
-  auto min_it = std::min_element(cleaned_cluster.begin(), cleaned_cluster.end());
-  error_rate = *min_it;
+  double smallest_cluster_snp = 1.0;
+  for(uint32_t k=0; k < largest_cluster_snps.size(); k++){
+    if(largest_cluster_snps[k] < smallest_cluster_snp){
+      smallest_cluster_snp = largest_cluster_snps[k];
+    }
+  }
+  error_rate = smallest_cluster_snp;
 }
