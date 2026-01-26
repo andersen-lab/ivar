12e9c7331de27512cbf37c1fb12585b0849e5ab7
gkarthik <xraydiffraction@gmail.com>
2026-01-26 13:24:44 -0800

    tests: fixed calling fit() with new arguments in test_bd_threshold.cpp

--- tests/test_bd_threshold.cpp
+++ tests/test_bd_threshold.cpp
@@ -20,12 +20,10 @@ int main() {
   double stdevs_diff[] = {0};
 
   std::ofstream f("/Users/karthik/tmp/test_bd_threshold/data/gaussian_samples_bd_threshold.tsv", std::ios::out);
-  f << "mean1\tmean2\tstdev1\tstdev2\tnsamples\tsample\tvalue\tvalue_logit\n";
+  f << "mean1\tmean2\tstdev1\tstdev2\tnsamples\tsample\tvalue\n";
 
   std::ofstream fresults("/Users/karthik/tmp/test_bd_threshold/output/inferred_gmm.tsv", std::ios::out);
-  fresults << "mean1\tmean2\tstdev1\tstdev2\tnsamples\tinferred_mean1_logit\tinferred_mean2_logit\tinferred_mean1\tinferred_mean2\tinferred_var1\tinferred_var2\tBD\tdistinct_components\n";
-
-
+  fresults << "mean1\tmean2\tstdev1\tstdev2\tnsamples\tinferred_mean1\tinferred_mean2\tinferred_var1\tinferred_var2\tBD\tdistinct_components\n";
 
   for(int nsamples: n_samples) {
     // Set number of sites
@@ -58,15 +56,20 @@ int main() {
               if (samples2[n] > 1.0) samples2[n] = 1-1e-2;
             }
 
-            // Logit transform
-            std::vector<double> samples1_logit;
-            std::vector<double> samples2_logit;
-            gmm_1d::logit_transform(samples1, samples1_logit);
-            gmm_1d::logit_transform(samples2, samples2_logit);
+            std::vector<double> samples;
 
-            std::vector<double> samples_logit;
-            samples_logit.insert(samples_logit.end(), samples1_logit.begin(), samples1_logit.end());
-            samples_logit.insert(samples_logit.end(), samples2_logit.begin(), samples2_logit.end());
+            samples.insert(samples.end(), samples1.begin(), samples1.end());
+            samples.insert(samples.end(), samples2.begin(), samples2.end());
+
+            // Logit transform
+//            std::vector<double> samples1_logit;
+//            std::vector<double> samples2_logit;
+//            gmm_1d::logit_transform(samples1, samples1_logit);
+//            gmm_1d::logit_transform(samples2, samples2_logit);
+
+//            std::vector<double> samples_logit;
+//            samples_logit.insert(samples_logit.end(), samples1_logit.begin(), samples1_logit.end());
+//            samples_logit.insert(samples_logit.end(), samples2_logit.begin(), samples2_logit.end());
 
             // Write to file
             for(int n = 0; n < nsamples;n++){
@@ -76,8 +79,8 @@ int main() {
                 << stdev2 << "\t"
                 << nsamples << "\t"
                 << "One" << "\t"
-                << samples1[n] << "\t"
-                << samples1_logit[n] << "\n";
+                << samples1[n] << "\n";
+//                << samples1_logit[n] << "\n";
             }
 
             for(int n = 0; n < nsamples;n++){
@@ -87,17 +90,19 @@ int main() {
                 << stdev2 << "\t"
                 << nsamples << "\t"
                 << "Two" << "\t"
-                << samples2[n] << "\t"
-                << samples2_logit[n] << "\n";
+                << samples2[n] << "\n";
+//                << samples2_logit[n] << "\n";
             }
 
             // Fit GMM
             std::vector<double> logL_history;
             gmm_1d model(2);
+            model.set_use_half_normal_for_noise(false);
             model.fit(
-                samples_logit,
+                samples,
                 sites,
                 logL_history,
+                {},
                 20,
                 1e-6
             );
@@ -118,8 +123,8 @@ int main() {
                      << nsamples << "\t"
                      << m[0] << "\t"
                      << m[1] << "\t"
-                     << m_sigmoid[0] << "\t"
-                     << m_sigmoid[1] << "\t"
+//                     << m_sigmoid[0] << "\t"
+//                     << m_sigmoid[1] << "\t"
                      << v[0] << "\t"
                      << v[1] << "\t"
                      << gmm_1d::calculate_bhattacharyya_distance_1d(m[0], v[0], m[1], v[1]) << "\t"
